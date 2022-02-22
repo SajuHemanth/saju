372f1e402f66ccfd78b2645da46ed87a20d118a8
SajuHemanth <96937846+SajuHemanth@users.noreply.github.com>
2022-02-22 20:11:05 +0530

    Create mapobjects.js

---
+++ mapobjects.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>JavaScript Map Objects</h2>
+<p>Objects as keys in a Map:</p>
+
+<p id="demo></p>
+
+<script>
+// Create Objects
+const watch = {name: 'watch'};
+const ring = {name: 'ring'};
+const chain = {name: 'chain'};
+//create new map
+const materials = new Map
+// Add the Objects to the Map
+materials.set(watch, 500);
+materials.set(ring, 300);
+materials.set(chain, 200);
+// Display Object Type
+document.getElementById("demo").innerHTML = materialss;
+</script>
+
+</body>
+</html>
